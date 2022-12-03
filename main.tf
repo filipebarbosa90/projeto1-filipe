67722ebaed194d97831921f6955ce3cf37a54801
Filipe Barbosa <filipe.barbosa@gmail.com>
2022-12-03 01:01:56 -0300

    Subindo Configuracoes

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
+# Create Resource group
 resource "azurerm_resource_group" "default" {
 name = "rg-filipe-iac"  
 location = "eastus"
@@ -19,4 +19,20 @@ tags = {
   "env" = "staging"
   "project": "filipe-barbosa"
 }
+}
+
+# Create Virtual network
+resource "azurerm_virtual_network" "ddfault" {
+  name = "vnet-filipe-iac"
+  address_space = ["10.0.0.0/16"]
+  location = "eastus"
+  resource_group_name = azurerm_resource_group.default.name
+
+}
+
+resource "azurerm_subnet" "internal" {
+  name = "default"
+  resource_group_name = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes = ["10.0.1.0/24"]
 }
